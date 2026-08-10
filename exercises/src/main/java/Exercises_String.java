d1ddd8795cc1e9dff7781bf89e914c02bb2b2702
Raphael Alexei <raphaeeelbr@gmail.com>
2026-08-10 19:11:35 -0300

    feat: new file with string exercises

---
+++ exercises/src/main/java/Exercises_String.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class Exercises_String {
+    public static void main(String[] args) {
+
+        String numero = "5";
+        int valor = Integer.parseInt(numero);
+        int resultado = valor + 10;
+
+        System.out.println(resultado);
+    }
+}
